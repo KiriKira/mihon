ff4519b82732c65c0c914374efb005e0f28a99f8
Kiri <32393884+KiriKira@users.noreply.github.com>
2026-09-19 01:33:43 +0900

    fix(reader): simplify foldable rotation dispatch

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
@@ -808,18 +808,15 @@ class ReaderActivity : BaseActivity() {
         val foldableRotation = if (isWideUnfoldedFoldable()) {
             when (newOrientation) {
                 ReaderOrientation.PORTRAIT,
-                ReaderOrientation.LOCKED_PORTRAIT,
-                -> -90f
+                ReaderOrientation.LOCKED_PORTRAIT -> -90f
 
                 ReaderOrientation.REVERSE_PORTRAIT -> 90f
 
                 ReaderOrientation.LANDSCAPE,
-                ReaderOrientation.LOCKED_LANDSCAPE,
-                -> 0f
+                ReaderOrientation.LOCKED_LANDSCAPE -> 0f
 
                 ReaderOrientation.DEFAULT,
-                ReaderOrientation.FREE,
-                -> null
+                ReaderOrientation.FREE -> null
             }
         } else {
             null
@@ -869,7 +866,7 @@ class ReaderActivity : BaseActivity() {
         if (!::binding.isInitialized) {
             return
         }
-        binding.readerContent.post(::applyReaderContentTransform)
+        binding.readerContent.post { applyReaderContentTransform() }
     }
 
     private fun applyReaderContentTransform() {
